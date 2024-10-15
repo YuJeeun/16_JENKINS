69858c630ed1f2144069c5074e6581880a19ea96
YuJeeun <dbwpdms1122@naver.com>
2024-10-15 12:18:34 +0900

    sdfsdf

--- src/main/java/com/ohgiraffers/jenkinsproject/controller/CalculatorController.java
+++ src/main/java/com/ohgiraffers/jenkinsproject/controller/CalculatorController.java
@@ -19,7 +19,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "I'm alive~!~!컄컄";
+        return "I'm alive~!~testsetsetsetset";
     }
 
     @GetMapping("/plus")
